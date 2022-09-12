564b0ea3faf25cdb135a30f83335b1fa7c8e3a93
Seeloewen <Louis.Herbert2006@gmail.com>
2022-09-12 22:32:20 +0200

    Move default profile selection to new settings window

---
+++ frmSettings.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSettings
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cbxDefaultProfile = New System.Windows.Forms.ComboBox()
+        Me.cbLoadProfileByDefault = New System.Windows.Forms.CheckBox()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cbxDefaultProfile
+        '
+        Me.cbxDefaultProfile.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbxDefaultProfile.FormattingEnabled = True
+        Me.cbxDefaultProfile.Location = New System.Drawing.Point(12, 38)
+        Me.cbxDefaultProfile.Name = "cbxDefaultProfile"
+        Me.cbxDefaultProfile.Size = New System.Drawing.Size(267, 21)
+        Me.cbxDefaultProfile.TabIndex = 12
+        '
+        'cbLoadProfileByDefault
+        '
+        Me.cbLoadProfileByDefault.AutoSize = True
+        Me.cbLoadProfileByDefault.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbLoadProfileByDefault.Location = New System.Drawing.Point(12, 12)
+        Me.cbLoadProfileByDefault.Name = "cbLoadProfileByDefault"
+        Me.cbLoadProfileByDefault.Size = New System.Drawing.Size(161, 20)
+        Me.cbLoadProfileByDefault.TabIndex = 11
+        Me.cbLoadProfileByDefault.Text = "Load profile by default:"
+        Me.cbLoadProfileByDefault.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancel.Location = New System.Drawing.Point(147, 70)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(133, 23)
+        Me.btnCancel.TabIndex = 13
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.Location = New System.Drawing.Point(12, 70)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(129, 23)
+        Me.btnSave.TabIndex = 14
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'frmSettings
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(292, 105)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.cbxDefaultProfile)
+        Me.Controls.Add(Me.cbLoadProfileByDefault)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmSettings"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Settings"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cbxDefaultProfile As ComboBox
+    Friend WithEvents cbLoadProfileByDefault As CheckBox
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btnSave As Button
+End Class
---
+++ frmSettings.vb
@@ -0,0 +1,68 @@
+﻿Imports System.IO
+
+Public Class frmSettings
+
+    'Variables used by the application to work correctly
+    Dim ProfileDirectory As String = frmMain.AppData + "\Profile Manager Demo\Profiles\" 'This is the directory, where the profile files are being stored
+    Dim ProfileList As String()
+
+    'This code should be implemented in your applications settings
+    Private Sub frmSettings_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Check checkbox depending on default profile setting
+        If My.Settings.LoadProfileByDefault Then
+            cbLoadProfileByDefault.Checked = True
+        ElseIf My.Settings.LoadProfileByDefault = False Then
+            cbLoadProfileByDefault.Checked = False
+        End If
+
+        'Clear profile list and get profile list for default profile combobox
+        cbxDefaultProfile.Items.Clear()
+        GetFiles(ProfileDirectory)
+
+        'Load default profile
+        cbxDefaultProfile.SelectedItem = My.Settings.DefaultProfile
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        'Close this window
+        Close()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        'Change default profile setting if checkbox is changed
+        If cbLoadProfileByDefault.Checked Then
+            My.Settings.LoadProfileByDefault = True
+        ElseIf cbLoadProfileByDefault.Checked = False Then
+            My.Settings.LoadProfileByDefault = False
+        End If
+
+        'Change default profile to combobox selection
+        My.Settings.DefaultProfile = cbxDefaultProfile.SelectedItem
+
+        'Show messagebox to confirm save
+        MsgBox("Saved settings!")
+    End Sub
+
+    Sub GetFiles(Path As String)
+        'Gets all the profile files from the directory and puts their name into the combobox
+        If Path.Trim().Length = 0 Then
+            Return
+        End If
+
+        ProfileList = Directory.GetFileSystemEntries(Path)
+
+        Try
+            For Each Profile As String In ProfileList
+                If Directory.Exists(Profile) Then
+                    GetFiles(Profile)
+                Else
+                    Profile = Profile.Replace(ProfileDirectory, "")
+                    Profile = Profile.Replace(".txt", "")
+                    cbxDefaultProfile.Items.Add(Profile)
+                End If
+            Next
+        Catch ex As Exception
+            MsgBox("Error: Could not load profiles. Please try again." + vbNewLine + "Exception: " + ex.Message)
+        End Try
+    End Sub
+End Class
